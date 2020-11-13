ad0d021fec38f64c1ae75d04f6e769722f958b23
Eduardo Lima <du.lima1337@gmail.com>
2020-11-13 15:27:19 -0300

    Updated Home page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Text from './pages/Text/Text';
 
 
 function App() {
+  let match = useRouteMatch();
+  console.log(match)
+
   return (
     <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/text" component={Text} exact />
+        <Route path={`${match.url}/`} component={Home} exact />
+        <Route path={`${match.url}text`} component={Text} exact />
     </Switch>
   )
 }
